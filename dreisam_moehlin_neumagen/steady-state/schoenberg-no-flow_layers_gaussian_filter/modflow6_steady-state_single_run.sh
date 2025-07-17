d003a5f8f4e337b233412c0b7f321c6a34132753
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-07-17 14:08:52 +0200

    modified sfr of dreisam-moehlin-neumagen catchment

---
+++ dreisam_moehlin_neumagen/steady-state/schoenberg-no-flow_layers_gaussian_filter/modflow6_steady-state_single_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# python add_schoenberg_mask.py
+# python add_drainage_mask.py
+# python modify_elevations.py
+# python modify_hydraulic_conductivity_and_specific_yield_of_layer1.py
+# python generate_boundary_conditions.py
+python write_fudge_parameters.py
+
+# python modflow6_steady-state.py --model-run 928
+# python evaluate_boundary_condition.py --model-run 928
+
+python modflow6_steady-state.py --model-run 928
+python write_binary_to_netcdf_steady-state.py --model-run 928
+# python evaluate_boundary_condition.py --model-run 928 --plot
+python plot_groundwater_heads_steady-state.py --model-run 928
+python evaluate_steady-state_simulation.py --model-run 928
