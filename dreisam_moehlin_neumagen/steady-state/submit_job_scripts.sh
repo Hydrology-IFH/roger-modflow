1c03c138d13efe40522c0abb96054b2689068db5
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-07-22 12:11:04 +0200

    modified sfr of dreisam-moehlin-neumagen catchment

--- dreisam_moehlin_neumagen/steady-state/submit_job_scripts.sh
+++ dreisam_moehlin_neumagen/steady-state/submit_job_scripts.sh
@@ -12,6 +12,10 @@ cd sfr-opt-kf-riv_drainage_schoenberg-no-flow_layers_gaussian_filter
 nohup ./modflow6_steady-state_monte_carlo.sh &
 cd ..
 
+cd sfr_drainage_schoenberg-no-flow_fudge4_gaussian_filter
+nohup ./modflow6_steady-state_monte_carlo.sh &
+cd ..
+
 cd sfr_drainage_schoenberg-no-flow_layers
 nohup ./modflow6_steady-state_monte_carlo.sh &
 cd ..
@@ -25,9 +29,5 @@ nohup ./modflow6_steady-state_monte_carlo.sh &
 cd ..
 
 cd schoenberg-no-flow_no-wells_layers_gaussian_filter
-nohup ./modflow6_steady-state_monte_carlo.sh &
-cd ..
-
-cd sfr_drainage_schoenberg-no-flow_fudge4_gaussian_filter
 nohup ./modflow6_steady-state_monte_carlo.sh &
 cd ..
