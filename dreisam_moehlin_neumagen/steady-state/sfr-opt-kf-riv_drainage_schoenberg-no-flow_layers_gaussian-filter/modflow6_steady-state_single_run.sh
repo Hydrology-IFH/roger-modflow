f8aeffb96b27e7727c4178ba6fe730ed8bee5131
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2025-09-29 17:53:01 +0200

    modified rhk initial value

--- dreisam_moehlin_neumagen/steady-state/sfr-opt-kf-riv_drainage_schoenberg-no-flow_layers_gaussian-filter/modflow6_steady-state_single_run.sh
+++ dreisam_moehlin_neumagen/steady-state/sfr-opt-kf-riv_drainage_schoenberg-no-flow_layers_gaussian-filter/modflow6_steady-state_single_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 python write_fudge_parameters.py
 
-# python modflow6_steady-state.py --model-run 5
-# python evaluate_boundary_condition.py --model-run 5
+# python modflow6_steady-state.py --model-run 7
+# python evaluate_boundary_condition.py --model-run 7
 
-python modflow6_steady-state.py --model-run 5
-python write_binary_to_netcdf_steady-state.py --model-run 5
-# python evaluate_boundary_condition.py --model-run 5 --plot
-python plot_groundwater_heads_steady-state.py --model-run 5
-python evaluate_steady-state_simulation.py --model-run 5
+python modflow6_steady-state.py --model-run 7
+python write_binary_to_netcdf_steady-state.py --model-run 7
+# python evaluate_boundary_condition.py --model-run 7 --plot
+python plot_groundwater_heads_steady-state.py --model-run 7
+python evaluate_steady-state_simulation.py --model-run 7
