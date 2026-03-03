f56735b7c13e6a0285f0d786e77158377bc22ca1
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-03-03 16:21:05 +0100

    added calculation of gw extraction balance

---
+++ dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_extraction_balance.py
@@ -0,0 +1,209 @@
+from pathlib import Path
+import numpy as np
+import xarray as xr
+import xesmf as xe
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import click
+
+def aggregate_to_coarser_resolution(vals, res_fine, res_coarse, method="sum", x_origin=0, y_origin=0):
+    """Aggregate raster data to a coarser resolution.
+    
+    Args
+    ----
+    vals : numpy.ndarray
+        2D array with the raster data.
+
+    res_fine : int
+        spatial resolution of the fine grid in meters.
+
+    res_coarse : int
+        spatial resolution of the coarse grid in meters.
+
+    method : str
+        Method to aggregate the data. Options are "sum" and "average".
+
+    x_origin : int
+        x-coordinate of the grid origin (lower left corner).
+
+    y_origin : int
+        y-coordinate of the grid origin (lower left corner).  
+    """
+    ny_fine, nx_fine = vals.shape[0], vals.shape[1]
+    nlat_coarse, nlon_coarse = int(res_coarse / res_fine), int(res_coarse / res_fine)
+    meters_to_latlon = 111195
+    lat_fine = np.linspace(y_origin, y_origin + ny_fine*(res_fine/meters_to_latlon), ny_fine)/meters_to_latlon  # boundaries
+    lon_fine = np.linspace(x_origin, x_origin + nx_fine*(res_fine/meters_to_latlon), nx_fine)/meters_to_latlon  # boundaries
+
+    arr_fine = xr.DataArray(vals, coords={"lat": lat_fine, "lon": lon_fine}, dims=["lat", "lon"])
+
+    if method == "sum":
+        arr_coarse = xr.DataArray(
+            arr_fine.coarsen(lat=nlat_coarse, lon=nlon_coarse).sum().values,
+            dims=("lat", "lon"),
+        )
+        
+    elif method == "average":
+        arr_coarse = xr.DataArray(
+            arr_fine.coarsen(lat=nlat_coarse, lon=nlon_coarse).mean().values,
+            dims=("lat", "lon"),
+        )
+    return arr_coarse.values
+
+
+@click.option("-mr", "--model-run", type=int, default=1806)
+@click.command("main", short_help="Evaluate the transient simulation")
+def main(model_run):
+    base_path = Path(__file__).parent
+
+    date_time = pd.date_range(start="2013-01-01", end="2023-12-31", freq="D")
+    years = np.unique(date_time.year.values)
+    timesteps = np.arange(len(date_time))
+
+    # load the indirect recharge
+    # click.echo("Loading indirect recharge...")
+    # ll_indirect_recharge = []
+    # for year in years:
+    #     output_file = base_path / "output" / "modflow_base-magnitude0-duration0_no-irrigation_no-yellow-mustard_soil-compaction" / f"indirect_recharge_run{model_run}_year{year}.nc"
+    #     ds_indirect_recharge = xr.open_dataset(output_file, engine="h5netcdf")
+    #     indirect_recharge_year = ds_indirect_recharge["indirect_recharge"].values
+    #     ll_indirect_recharge.append(indirect_recharge_year)
+    # indirect_recharge = np.concatenate(ll_indirect_recharge, axis=0)
+    # # create xarray data array for indirect recharge
+    # da_indirect_recharge = xr.DataArray(
+    #     data=indirect_recharge,
+    #     dims=["time", "y", "x"],
+    #     coords={
+    #         "time": date_time,
+    #         "y": ds_indirect_recharge["y"].values,
+    #         "x": ds_indirect_recharge["x"].values,
+    #     },
+    # )
+    # # resample to monthly
+    # da_indirect_recharge_monthly = da_indirect_recharge.resample(time="ME").sum()
+    # # dataframe with monthly total sum of indirect recharge
+    # df_indirect_recharge_monthly = pd.DataFrame(index=da_indirect_recharge_monthly.time.values, data=da_indirect_recharge_monthly.sum(dim=["y", "x"]).values, columns=["indirect_recharge"])
+    # # resample to annual
+    # da_indirect_recharge_annual = da_indirect_recharge.resample(time="YE").sum()
+    # # dataframe with annual total sum of indirect recharge
+    # df_indirect_recharge_annual = pd.DataFrame(index=da_indirect_recharge_annual.time.values, data=da_indirect_recharge_annual.sum(dim=["y", "x"]).values, columns=["indirect_recharge"])
+
+    # load direct recharge
+    click.echo("Loading direct recharge...")
+    ll_direct_recharge = []
+    for year in years:
+        base_path_roger = base_path.parent.parent.parent.parent / "roger"
+        output_file = base_path_roger / "examples" / "catchment_scale" / "dreisam_moehlin_neumagen" / "oneD_crop_distributed" / f"recharge_base-magnitude0-duration0_no-irrigation_no-yellow-mustard_soil-compaction_year{year}.nc"
+        ds_direct_recharge = xr.open_dataset(output_file, engine="h5netcdf")
+        _direct_recharge_year = ds_direct_recharge["direct_recharge"].values
+        for i in range(_direct_recharge_year.shape[0]):
+            direct_recharge_day = aggregate_to_coarser_resolution(_direct_recharge_year[i, :, :], 25, 50, method="average")
+            ll_direct_recharge.append(direct_recharge_day)
+    direct_recharge = np.concatenate(ll_direct_recharge, axis=0)
+    # convert from mm/day to m3/day
+    # get the area of each grid cell in m2
+    area = 50 * 50  # 50 m x 50 m grid cells
+    # multiply direct recharge by area to get m3/day
+    direct_recharge = direct_recharge * area / 1000  # convert mm to m
+    # create xarray data array for direct recharge
+    da_direct_recharge = xr.DataArray(
+        data=direct_recharge,
+        dims=["time", "y", "x"],
+        coords={
+            "time": date_time,
+            "y": ds_direct_recharge["y"].values,
+            "x": ds_direct_recharge["x"].values,
+        },
+    )
+    # resample to monthly
+    da_direct_recharge_monthly = da_direct_recharge.resample(time="ME").sum()
+    # dataframe with monthly total sum of direct recharge
+    df_direct_recharge_monthly = pd.DataFrame(index=da_direct_recharge_monthly.time.values, data=da_direct_recharge_monthly.sum(dim=["y", "x"]).values, columns=["direct_recharge"])
+
+    # resample to annual
+    da_direct_recharge_annual = da_direct_recharge.resample(time="YE").sum()
+    # dataframe with annual total sum of direct recharge
+    df_direct_recharge_annual = pd.DataFrame(index=da_direct_recharge_annual.time.values, data=da_direct_recharge_annual.sum(dim=["y", "x"]).values, columns=["direct_recharge"])
+
+    # # create xarray data array for total recharge
+    # da_recharge = da_indirect_recharge + da_direct_recharge
+    # # resample to monthly
+    # da_recharge_monthly = da_recharge.resample(time="ME").sum()
+    # # resample to annual
+    # da_recharge_annual = da_recharge.resample(time="YE").sum()
+    # df_recharge_monthly = df_indirect_recharge_monthly + df_direct_recharge_monthly
+    # df_recharge_monthly.columns = ["recharge"]
+    # df_recharge_annual = df_indirect_recharge_annual + df_direct_recharge_annual
+    # df_recharge_annual.columns = ["recharge"]
+
+    # load well extraction
+    click.echo("Loading well extraction...")
+    # save daily drinking water well extraction to csv
+    file = base_path / "input" / "drinking_water_well_extraction_daily.csv"
+    df_drinking_water_well_extraction_daily = pd.read_csv(file, sep=";", index_col=0)
+    df_drinking_water_well_extraction_daily.index = pd.to_datetime(df_drinking_water_well_extraction_daily.index, format="%Y-%m-%d")
+    # resample to monthly
+    df_drinking_water_well_extraction_monthly = df_drinking_water_well_extraction_daily.resample("ME").sum()
+    # resample to annual
+    df_drinking_water_well_extraction_annual = df_drinking_water_well_extraction_daily.resample("YE").sum()
+
+    # calculate the extraction balance as the difference between the drinking water well extraction and the indirect recharge multiplied by 0.3 (assuming that 30% of the indirect recharge is available for extraction)
+    df_extraction_balance_monthly = df_drinking_water_well_extraction_monthly - (df_direct_recharge_monthly * 0.3)
+    df_extraction_balance_annual = df_drinking_water_well_extraction_annual - (df_direct_recharge_annual * 0.3)
+
+    # make figures directory if it does not exist
+    figures_dir = base_path / "figures" / "gw_extraction_balance"
+    figures_dir.mkdir(exist_ok=True)
+
+    # # plot monthly direct and indirect recharge using stacked bar plot use blue for direct recharge and purple for indirect recharge 
+    # fig, ax = plt.subplots(figsize=(6, 2))
+    # df_recharge_monthly_stacked = df_direct_recharge_monthly.copy()
+    # df_recharge_monthly_stacked["indirect"] = df_indirect_recharge_monthly["indirect_recharge"]
+    # df_recharge_monthly_stacked.plot(kind="bar", stacked=True, ax=ax, color=["blue", "purple"])
+    # ax.set_xlabel("Jahr")
+    # ax.set_ylabel("Recharge [m³/Monat]")
+    # fig.tight_layout()
+    # fig.savefig(figures_dir / "recharge_monthly_stacked.png", dpi=300)
+
+    # # plot annual direct and indirect recharge using stacked bar plot use blue for direct recharge and purple for indirect recharge
+    # fig, ax = plt.subplots(figsize=(6, 2))
+    # df_recharge_annual_stacked = df_direct_recharge_annual.copy()
+    # df_recharge_annual_stacked["indirect"] = df_indirect_recharge_annual["indirect_recharge"]
+    # df_recharge_annual_stacked.plot(kind="bar", stacked=True, ax=ax, color=["blue", "purple"])
+    # ax.set_xlabel("Jahr")
+    # ax.set_ylabel("Recharge [m³/Jahr]")
+    # fig.tight_layout()
+    # fig.savefig(figures_dir / "recharge_annual_stacked.png", dpi=300)
+
+    # plot the monthly extraction balance using bar plot, make bars with negative values orange and bars with positive values blue
+    fig, ax = plt.subplots(figsize=(6, 2))
+    colors = ["orange" if x < 0 else "blue" for x in df_extraction_balance_monthly["drinking_water_well_extraction"]]
+    ax.bar(df_extraction_balance_monthly.index, df_extraction_balance_monthly["drinking_water_well_extraction"], color=colors)
+    ax.set_xlabel("Jahr")
+    ax.set_ylabel("GW-Entnahmebilanz [m³/day]")
+    fig.tight_layout()
+    fig.savefig(figures_dir / "extraction_balance_monthly.png", dpi=300)
+
+    # plot the annual extraction balance using bar plot, make bars with negative values orange and bars with positive values blue
+    fig, ax = plt.subplots(figsize=(6, 2))
+    colors = ["orange" if x < 0 else "blue" for x in df_extraction_balance_annual["drinking_water_well_extraction"]]
+    ax.bar(df_extraction_balance_annual.index, df_extraction_balance_annual["drinking_water_well_extraction"], color=colors)
+    ax.set_xlabel("Jahr")
+    ax.set_ylabel("GW-Entnahmebilanz [m³/day]")
+    fig.tight_layout()
+    fig.savefig(figures_dir / "extraction_balance_annual.png", dpi=300)
+
+    # plot the long-term balance of the extraction balance using a bar plot, make bars with negative values orange and bars with positive values blue
+    long_term_balance = df_extraction_balance_annual["drinking_water_well_extraction"].sum()
+    fig, ax = plt.subplots(figsize=(2, 2))
+    color = "orange" if long_term_balance < 0 else "blue"
+    ax.bar(["Long-term balance"], [long_term_balance], color=color)
+    ax.set_ylabel("GW-Entnahmebilanz [m³/day]")
+    fig.tight_layout()
+    fig.savefig(figures_dir / "extraction_balance_long_term.png", dpi=300)
+
+    return
+
+if __name__ == "__main__":
+    main()
