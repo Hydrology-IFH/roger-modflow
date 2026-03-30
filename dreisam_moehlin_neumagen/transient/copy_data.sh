68786dae42b955d5472736f70df2ff1784334345
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-03-30 15:12:54 +0200

    copy data to PROJECT

--- dreisam_moehlin_neumagen/transient/copy_data.sh
+++ dreisam_moehlin_neumagen/transient/copy_data.sh
@@ -10,4 +10,4 @@
 #SBATCH --error=copy_data_err.out
 #SBATCH --export=ALL
 
-cp -r /pfs/10/work/fr_rs1092-workspace/roger-modflow/dreisam_moehlin_neumagen_porous/transient/offline_coupling/output/* /pfs/10/project/bw22g004/fr_rs1092/workspace-1773831854/roger-modflow/dreisam_moehlin_neumagen_porous/transient/offline_coupling/output/.
+cp -r /pfs/10/work/fr_rs1092-workspace/roger-modflow/dreisam_moehlin_neumagen/transient/offline_coupling/output/* /pfs/10/project/bw22g004/fr_rs1092/workspace-1773831854/roger-modflow/dreisam_moehlin_neumagen/transient/offline_coupling/output/.
