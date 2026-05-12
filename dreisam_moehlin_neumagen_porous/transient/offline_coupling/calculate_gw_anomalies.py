dde0ce025e9cb490eb813ec242c971100c2717c9
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-05-12 09:24:18 +0200

    modified plots

--- dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_anomalies.py
+++ dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_anomalies.py
@@ -66,7 +66,7 @@ def main(model_run):
     #                          "summer-drought-magnitude0-duration3_irrigation_no-yellow-mustard_soil-compaction",
     #                          "summer-drought-magnitude0-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
     
-    stress_test_scenarios = ["summer-drought-magnitude2-duration3_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"
+    stress_test_scenarios = ["summer-drought-magnitude2-duration3_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
                              "summer-drought-magnitude2-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
                              "long-term-magnitude2-duration2_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
                              "long-term-magnitude2-duration2_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
--- dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_recharge_anomalies.py
+++ dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_recharge_anomalies.py
@@ -60,7 +60,7 @@ def main(model_run):
 
     base = "base-magnitude0-duration0_no-irrigation_no-yellow-mustard_soil-compaction"
 
-    stress_test_scenarios = ["summer-drought-magnitude2-duration3_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"
+    stress_test_scenarios = ["summer-drought-magnitude2-duration3_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
                              "summer-drought-magnitude2-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
                              "long-term-magnitude2-duration2_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
                              "long-term-magnitude2-duration2_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
