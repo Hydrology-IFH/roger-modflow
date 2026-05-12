82ea51974a9fea3f75b8c91321c8fbf682d0a726
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-05-12 09:50:26 +0200

    modified plots

--- dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_anomalies.py
+++ dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_anomalies.py
@@ -66,10 +66,8 @@ def main(model_run):
     #                          "summer-drought-magnitude0-duration3_irrigation_no-yellow-mustard_soil-compaction",
     #                          "summer-drought-magnitude0-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
     
-    stress_test_scenarios = ["summer-drought-magnitude2-duration3_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
-                             "summer-drought-magnitude2-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
-                             "long-term-magnitude2-duration2_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
-                             "long-term-magnitude2-duration2_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
+    stress_test_scenarios = ["long-term-magnitude2-duration0_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
+                             "long-term-magnitude2-duration0_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
     
     date_time = pd.date_range(start="2013-01-01", end="2023-12-31", freq="D")
     years = np.unique(date_time.year.values)
--- dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_extraction_balance.py
+++ dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_extraction_balance.py
@@ -60,10 +60,8 @@ def main(model_run):
 
     areas = ["dmn", "wsg_hausen", "wsg_zartener_becken"]
 
-    stress_test_scenarios = ["summer-drought-magnitude2-duration3_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
-                             "summer-drought-magnitude2-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
-                             "long-term-magnitude2-duration2_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
-                             "long-term-magnitude2-duration2_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
+    stress_test_scenarios = ["long-term-magnitude2-duration0_no-irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress",
+                             "long-term-magnitude2-duration0_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
     
     date_time = pd.date_range(start="2013-01-01", end="2023-12-31", freq="D")
     years = np.unique(date_time.year.values)
