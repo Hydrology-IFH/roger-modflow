42c016508725b3b52d2d6920cffe7d80b1d0737c
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2026-05-12 08:49:16 +0200

    modified plots

--- dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_extraction_balance.py
+++ dreisam_moehlin_neumagen_porous/transient/offline_coupling/calculate_gw_extraction_balance.py
@@ -60,9 +60,8 @@ def main(model_run):
 
     areas = ["dmn", "wsg_hausen", "wsg_zartener_becken"]
 
-    stress_test_scenarios = ["base-magnitude0-duration0_no-irrigation_no-yellow-mustard_soil-compaction",
-                             "summer-drought-magnitude0-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
-    
+    stress_test_scenarios = ["summer-drought-magnitude2-duration3_irrigation_no-yellow-mustard_soil-compaction",
+                             "summer-drought-magnitude2-duration3_irrigation_no-yellow-mustard_soil-compaction_well-extraction-stress"]
     
     date_time = pd.date_range(start="2013-01-01", end="2023-12-31", freq="D")
     years = np.unique(date_time.year.values)
